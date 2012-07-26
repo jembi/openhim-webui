498ef18affc9ec499fd246dcf84dd523fec2728c
Kari Schoonbee <kari@jembi.org>
2012-07-26 11:54:29 +0000

    ErrorUI: added trigger on database to set unreviewed status if error transaction is received. Only display ! icons (instead of ?) if transaction got an error and is unreviewed.

--- resources/update_database.sql
+++ resources/update_database.sql
@@ -32,3 +32,15 @@ insert into users_roles values ('2', '2');
 
 ALTER TABLE transaction_log ADD flagged BOOLEAN;
 ALTER TABLE transaction_log ADD reviewed BOOLEAN;
+
+DELIMITER |
+CREATE TRIGGER update_reviewed 
+BEFORE INSERT ON `transaction_log` 
+FOR EACH ROW 
+BEGIN 
+IF NEW.status = 3 THEN	
+SET NEW.reviewed = 0;
+END IF; 
+END;
+|
+DELIMITER ;
