eff12f5dcae4b785df1824337018ed5b8ccbf118
Ryan Crichton <rg.crichton@gmail.com>
2012-07-26 15:02:22 +0000

    Renaming database file, adding use database command.

--- resources/update_database_3.sql
+++ resources/update_database_3.sql
@@ -3,9 +3,11 @@ use interoperability_layer;
 ALTER TABLE transaction_log ADD flagged BOOLEAN;
 ALTER TABLE transaction_log ADD reviewed BOOLEAN;
 
+UPDATE transaction_log SET reviewed = 0 WHERE status = 3 AND reviewed = NULL;
+
 DELIMITER |
 CREATE TRIGGER update_reviewed 
-BEFORE INSERT ON `transaction_log` 
+BEFORE UPDATE ON `transaction_log` 
 FOR EACH ROW 
 BEGIN 
 IF NEW.status = 3 THEN	
